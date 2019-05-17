aecadfcffdec3e6704d844df63f3131c08e8d3ec
Jorge Fernando Rincón Figueroa <jofer86@gmail.com>
2019-05-17 17:52:47 -0500

    add model for Post and assosiations for Posts to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_many :posts
   validates :name, presence: true, length: {minimum: 5}
   VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence:true, format: {with: VALID_EMAIL_REGEX}
---
+++ db/migrate/20190517223945_add_user_id_to_posts.rb
@@ -0,0 +1,5 @@
+class AddUserIdToPosts < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :posts, :user
+  end
+end
